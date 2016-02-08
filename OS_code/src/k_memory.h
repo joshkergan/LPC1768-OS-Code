bdcab2b305e4e8ec71c92a09adde773cdc8d4492
Josh Kergan <joshkergan@gmail.com>
2016-02-08 00:05:18 -0500

    Moved code into top level folder

---
+++ OS_code/src/k_memory.h
@@ -0,0 +1,33 @@
+/**
+ * @file:   k_memory.h
+ * @brief:  kernel memory managment header file
+ * @author: Yiqing Huang
+ * @date:   2014/01/17
+ */
+ 
+#ifndef K_MEM_H_
+#define K_MEM_H_
+
+#include "k_rtx.h"
+
+/* ----- Definitions ----- */
+#define RAM_END_ADDR 0x10008000
+
+/* ----- Variables ----- */
+/* This symbol is defined in the scatter file (see RVCT Linker User Guide) */  
+extern unsigned int Image$$RW_IRAM1$$ZI$$Limit; 
+extern PCB **gp_pcbs;
+extern QUEUE *gp_pqueue;
+extern PROC_INIT g_proc_table[NUM_TEST_PROCS];
+extern U32 g_num_blocked;
+extern U32 g_released_memory;
+extern PCB *gp_current_process;
+extern int k_release_processor(void);
+
+/* ----- Functions ------ */
+void memory_init(void);
+U32 *alloc_stack(U32 size_b);
+void *k_request_memory_block(void);
+int k_release_memory_block(void *);
+
+#endif /* ! K_MEM_H_ */
